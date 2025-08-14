06f1b9d2db4a24f38a2a482a1bd9b0fbaa8dc6c4
5ock <ccrubio5@gmail.com>
2025-08-14 13:47:10 +0900

    .

--- medium/3. Longest Substring Without Repeating Characters.ts
+++ medium/3. Longest Substring Without Repeating Characters.ts
@@ -1,37 +1,17 @@
-/**
- * 
-    Given a string s, find the length of the longest substring without duplicate characters.
-
-    Example 1:
-        Input: s = "abcabcbb"
-        Output: 3
-        Explanation: The answer is "abc", with the length of 3.
-
-    Example 2:
-        Input: s = "bbbbb"
-        Output: 1
-        Explanation: The answer is "b", with the length of 1.
-
-    Example 3:
-        Input: s = "pwwkew"
-        Output: 3
-        Explanation: The answer is "wke", with the length of 3.
-        Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
- * 
- */
-
-function lengthOfLongestSubstring(s: string): number {
-    let start = 0;
-    let maxLength = 0;
-    const charMap: Map<string, number> = new Map<string, number>();
-
-    for(let end = 0; end < s.length; end++) {
-        if(charMap.has(s[end]) && charMap.get(s[end])! >= start)
-            start = charMap.get(s[end])! + 1;
-
-        charMap.set(s[end], end);
-        maxLength = Math.max(maxLength, end - start + 1);
+function subarraySum(nums: number[], k: number): number {
+    const prefixSumMap = new Map<number, number>();
+    prefixSumMap.set(0, 1);
+    
+    let count = 0;
+    let sum = 0;
+    for(const num of nums) {
+        sum += num;
+        
+        if(prefixSumMap.has(sum - k))
+            count += prefixSumMap.get(sum - k);
+        
+        prefixSumMap.set(sum, (prefixSumMap.get(sum) || 0) + 1);
     }
-
-    return maxLength;
+    
+    return count;
 };
---
+++ medium/560. Subarray Sum Equals K.ts
@@ -0,0 +1,32 @@
+/**
+ * 
+    Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+    A subarray is a contiguous non-empty sequence of elements within an array.
+
+    Example 1:
+        Input: nums = [1,1,1], k = 2
+        Output: 2
+    Example 2:
+        Input: nums = [1,2,3], k = 3
+        Output: 2
+ * 
+ */
+
+function subarraySum(nums: number[], k: number): number {
+    const prefixSumMap = new Map<number, number>();
+    prefixSumMap.set(0, 1);
+
+    let count = 0;
+    let sum = 0;
+    for(const num of nums) {
+        sum += num;
+
+        if(prefixSumMap.has(sum - k))
+            count += prefixSumMap.get(sum - k)!;
+
+        prefixSumMap.set(sum, (prefixSumMap.get(sum) || 0) + 1);
+    }
+
+    return count;
+};

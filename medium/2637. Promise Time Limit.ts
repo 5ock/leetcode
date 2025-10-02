462061381238c064e39573a1c63004459826cb78
5ock <ccrubio5@gmail.com>
2025-10-02 10:27:28 +0900

    .

--- medium/2637. Promise Time Limit.ts
+++ medium/2637. Promise Time Limit.ts
@@ -57,6 +57,19 @@ function timeLimit(fn: Fn, t: number): Fn {
     }
 };
 
+// Promise.race
+
+function timeLimit_promise_race(fn: Fn, t: number): Fn {
+    return async function(...args) {
+        return Promise.race([
+            fn(...args),
+            new Promise((_, reject) =>
+                setTimeout(() => reject('Time Limit Exceeded'), t)
+            )
+        ])
+    }
+}
+
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
  * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms

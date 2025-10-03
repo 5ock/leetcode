faa76265d75aef05bbf013f7f4e2ac171fac76a5
5ock <ccrubio5@gmail.com>
2025-10-03 13:26:21 +0900

    .

---
+++ medium/2705. Compact Object.ts
@@ -0,0 +1,53 @@
+export {}
+
+/**
+ * 
+Given an object or array obj, return a compact object.
+
+A compact object is the same as the original object, except with keys containing falsy values removed. This operation applies to the object and any nested objects. Arrays are considered objects where the indices are keys. A value is considered falsy when Boolean(value) returns false.
+
+You may assume the obj is the output of JSON.parse. In other words, it is valid JSON.
+
+Example 1:
+    Input: obj = [null, 0, false, 1]
+    Output: [1]
+    Explanation: All falsy values have been removed from the array.
+Example 2:
+    Input: obj = {"a": null, "b": [false, 1]}
+    Output: {"b": [1]}
+    Explanation: obj["a"] and obj["b"][0] had falsy values and were removed.
+Example 3:
+    Input: obj = [null, 0, 5, [0], [false, 16]]
+    Output: [5, [], [16]]
+    Explanation: obj[0], obj[1], obj[3][0], and obj[4][0] were falsy and removed.
+ * 
+ */
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function compactObject(obj: Obj): Obj {
+    if(Array.isArray(obj)) {
+        const res: any[] = [];
+        for(const item of obj) {
+            if(item) {
+                if(typeof item === 'object' && item !== null)
+                    res.push(compactObject(item));
+                else
+                    res.push(item);
+            }
+        }
+        return res;
+    } else {
+        const res: {} = {};
+        for(const [k, v] of Object.entries(obj)) {
+            if(v) {
+                if(typeof v === 'object' && v !== null)
+                    res[k] = compactObject(v);
+                else
+                    res[k] = v;
+            }
+        }
+        return res;
+    }
+};
